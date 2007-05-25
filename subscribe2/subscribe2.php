e5f14c8a8c6bf6fd72dd69f543c384eab610de22
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-25 18:54:48 +0000

    Alteration of version numbers and renumbering of tagged releases
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/branches/WordPressMuand2.1@13279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subscribe2/subscribe2.php
+++ subscribe2/subscribe2.php
@@ -3,7 +3,7 @@
 Plugin Name: Subscribe2
 Plugin URI: http://subscribe2.wordpress.com
 Description: Notifies an email list when new entries are posted. Tested with WordPress 2.1.x and 2.2.x.
-Version: 2.3.5
+Version: 3.6
 Author: Matthew Robinson
 Author URI: http://subscribe2.wordpress.com
 */
@@ -41,7 +41,7 @@ define('S2PAGE', '0');
 
 // our version number. Don't touch this or any line below
 // unless you know exacly what you are doing
-define('S2VERSION', '2.3.5');
+define('S2VERSION', '3.6');
 
 // use Owen's excellent ButtonSnap library
 require(ABSPATH . '/wp-content/plugins/buttonsnap.php');
@@ -245,13 +245,13 @@ class s2class {
 		$headers .= "Precedence: list\nList-Id: " . get_settings('blogname') . "\n";
 
 		if ('html' == $type) {
-				// To send HTML mail, the Content-type header must be set
+				// To send HTML mail, the Content-Type header must be set
 				$headers .= "MIME-Version: 1.0\n";
-				$headers .= "Content-type: " . get_bloginfo('html_type') . "; charset=\"". get_bloginfo('charset') . "\"\n";
+				$headers .= "Content-Type: " . get_bloginfo('html_type') . "; charset=\"". get_bloginfo('charset') . "\"\n";
 				$mailtext = "<html><head><title>" . $subject . "</title></head><body>" . $message . "</body></html>";
 		} else {
 				$headers .= "MIME-Version: 1.0\n";
-				$headers .= "Content-type: text/plain; charset=\"". get_bloginfo('charset') . "\"\n";
+				$headers .= "Content-Type: text/plain; charset=\"". get_bloginfo('charset') . "\"\n";
 				$message = preg_replace('|&[^a][^m][^p].{0,3};|', '', $message);
 				$message = preg_replace('|&amp;|', '&', $message);
 				$mailtext = wordwrap(strip_tags($message), 80, "\n");
@@ -525,7 +525,7 @@ class s2class {
 		$mailheaders .= "X-Mailer:PHP" . phpversion() . "\n";
 		$mailheaders .= "Precedence: list\nList-Id: " . get_settings('blogname') . "\n";
 		$mailheaders .= "MIME-Version: 1.0\n";
-		$mailheaders .= "Content-type: text/plain; charset=\"". get_bloginfo('charset') . "\"\n";
+		$mailheaders .= "Content-Type: text/plain; charset=\"". get_bloginfo('charset') . "\"\n";
 
 		@wp_mail ($this->email, $subject, $body, $mailheaders);
 	} // end send_confirm()
