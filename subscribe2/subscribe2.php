335a3d4d1efdd5d61158d27d9df37e6150103253
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-01-10 19:14:25 +0000

    Change Ravan to RavanH in most recent docs
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@28922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subscribe2/subscribe2.php
+++ subscribe2/subscribe2.php
@@ -3,7 +3,7 @@
 Plugin Name: Subscribe2
 Plugin URI: http://subscribe2.wordpress.com
 Description: Notifies an email list when new entries are posted.
-Version: 4.3
+Version: 4.4
 Author: Matthew Robinson
 Author URI: http://subscribe2.wordpress.com
 */
@@ -41,7 +41,7 @@ define('S2PAGE', '0');
 
 // our version number. Don't touch this or any line below
 // unless you know exacly what you are doing
-define('S2VERSION', '4.3');
+define('S2VERSION', '4.4');
 define ('S2PATH', trailingslashit(dirname(__FILE__)));
 
 // use Owen's excellent ButtonSnap library
