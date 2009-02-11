bcefce6d12c58a395b5de1046f1327d7e5945151
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-11 19:33:59 +0000

    Add i18n string and stop s2mail being updated if empty
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@93759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subscribe2.php
+++ subscribe2.php
@@ -2189,7 +2189,7 @@ class s2class {
 			}
 			echo " /> " . $value['display'] . "<br />\r\n";
 		}
-		echo "<br />" . __('Send Digest Notification at', 'subscribe2') . ": \r\n";
+		echo "<br />" . __('Send Digest Notification at', 'subscribe2', 'subscribe2') . ": \r\n";
 		$hours = array('12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm');
 		echo "<select name=\"hour\">\r\n";
 		while ($hour = current($hours)) {
@@ -2309,7 +2309,9 @@ class s2class {
 			if (!current_user_can('edit_post', $post_id)) { return $post_id; }
 		}
 
- 		update_post_meta($post_id, 's2mail', $_POST['s2_meta_field']);
+ 		if ($_POST['s2_meta_field'] == 'yes') {
+ 			update_post_meta($post_id, 's2mail', $_POST['s2_meta_field']);
+ 		}
 	} // end s2_meta_box_handler()
 
 /* ===== template and filter functions ===== */
