9f87ad4ee9f1e3ce3e764ab80a25a493d360d8eb
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-11 21:30:45 +0000

    Tag and release 5.6
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/tags/5.6@216352 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/s2_edit.js
+++ include/s2_edit.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
 	// hide our span before page loads
 	jQuery('#s2bcc_2').hide();
 	jQuery('#s2page_2').hide();
+	jQuery('#s2entries_2').hide();
 });
 
 //show span on clicking the edit link
@@ -19,15 +20,15 @@ function s2_hide(id) {
 };
 
 function s2_update(id) {
-	var input = jQuery('input[@name="'+id+'"]').val();
-	jQuery('input[@name="'+id+'"]').val(input);
+	var input = jQuery('input[name="'+id+'"]').val();
+	jQuery('input[name="'+id+'"]').val(input);
 	jQuery('#s2'+id).html(input);
 	s2_hide(id);
 };
 
 function s2_revert(id) {
 	var option = jQuery('#js'+id).val();
-	jQuery('input[@name="'+id+'"]').val(option);
+	jQuery('input[name="'+id+'"]').val(option);
 	jQuery('#s2'+id).html(option);
 	s2_hide(id);
 };
