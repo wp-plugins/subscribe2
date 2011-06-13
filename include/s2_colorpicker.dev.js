7300ee984db93a73166eaaea55309d3915355ba8
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 16:47:35 +0000

    jQuery syntax corrections
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@396766 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/s2_colorpicker.dev.js
+++ include/s2_colorpicker.dev.js
@@ -2,42 +2,42 @@
 // version 1.1 - Split into separate js file and amended
 // version 1.2 - Improvements to the code for the colorpicker
 // version 1.3 - Got colorpicker working without a page reload using .live event
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 	var id;
 	jQuery('.colorpickerField')
 	// show the colorpicker when the class is focused
-	.live('focusin', function() {
+	.live('focusin', function () {
 		id = this;
 		jQuery(this).ColorPicker({
 			// define some ColorPicker events
-			onBeforeShow: function() {
+			onBeforeShow: function () {
 				jQuery(this).ColorPickerSetColor(this.value);
 			},
-			onShow: function(el) {
+			onShow: function (el) {
 				jQuery(el).fadeIn(500);
 				return false;
 			},
-			onHide: function(el) {
+			onHide: function (el) {
 				jQuery(el).fadeOut(500);
 				return false;
 			},
-			onChange: function(hsb, hex, rgb) {
+			onChange: function (hsb, hex, rgb) {
 				a = hex.toUpperCase();
 				id.value = a;
 			},
-			onSubmit: function(hsb, hex, rgb, el) {
+			onSubmit: function (hsb, hex, rgb, el) {
 				a = hex.toUpperCase();
 				id.value = a;
 				jQuery('.colorpicker').fadeOut(500);
 				return false;
 			}
-		})
+		});
 		// bind the colorpicker to keyboard input
-		jQuery(this).keyup(function(){
-			if (this.value.length == 6) {
+		jQuery(this).keyup(function () {
+			if (this.value.length === 6) {
 				id.value = this.value.toUpperCase();
 				jQuery(this).ColorPickerSetColor(id.value);
 			}
-		})
-	})
+		});
+	});
 });
