d94ef5a4785ee6e97621d8815f5ec83ff7ef7f6d
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-31 20:58:21 +0000

    Add class to allow hiding of Authors section
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@498161 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subscribe2.php
+++ subscribe2.php
@@ -2554,8 +2554,10 @@ class s2class {
 		}
 
 		if ( count($this->get_authors()) > 1 ) {
+			echo "<div class=\"s2_admin\" id=\"s2_authors\">\r\n";
 			echo "<h2>" . __('Do not send notifications for post made by these authors', 'subscribe2') . "</h2>\r\n";
 			$this->display_author_form(explode(',', get_user_meta($user_ID, $this->get_usermeta_keyname('s2_authors'), true)));
+			echo "</div>\r\n";
 		}
 
 		// submit
