bddc393c00d54c0d2e3ed4ad7428b5f891638a0d
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-17 18:46:00 +0000

    Improve efficiency of Farbtastic code init by linking to focus rather than mousemove
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@532466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/s2_colorpicker.dev.js
+++ include/s2_colorpicker.dev.js
@@ -19,27 +19,28 @@ jQuery(document).ready(function() {
 		});
 	} else {
 		// use .on as we are using jQuery 1.7 and up where .live is deprecated
-		jQuery(document).on('mousemove', function(event){
-			jQuery('.colorpickerField').each(function(){
-				if ( jQuery(this).attr('id').search("__i__") === -1 ) {
-					var picker;
-					var field = jQuery(this).attr('id').substr(0,20);
-					jQuery('.s2_colorpicker').each(function() {
-						if ( jQuery(this).attr('id').search(field) !== -1) {
-							picker = jQuery(this).attr('id');
-
-						}
-					});
-					jQuery(this).on('click', function(event) {
-						jQuery('.s2_colorpicker').hide();
-						jQuery.farbtastic('#' + picker).linkTo(this);
-						jQuery('#' + picker).slideDown();
-					});
+		jQuery(document).on('focus', '.colorpickerField',function(){
+			if (jQuery(this).is('.s2_initialised') || this.id.search('__i__') !== -1) {
+				return; // exit early, already initialized or not activated
+			}
+			jQuery(this).addClass('s2_initialised');
+			var picker;
+			var field = jQuery(this).attr('id').substr(0,20);
+			jQuery('.s2_colorpicker').each(function() {
+				if ( jQuery(this).attr('id').search(field) !== -1) {
+					picker = jQuery(this).attr('id');
+					return false; // stop looping
 				}
 			});
-			jQuery('.colorpickerField').focusout(function() {
-				jQuery('.s2_colorpicker').slideUp();
+			jQuery(this).on('focusin', function(event) {
+				jQuery('.s2_colorpicker').hide();
+				jQuery.farbtastic('#' + picker).linkTo(this);
+				jQuery('#' + picker).slideDown();
 			});
+			jQuery(this).on('focusout', function(event) {
+				jQuery('#' + picker).slideUp();
+			});
+			jQuery(this).trigger('focus'); // retrigger focus event for plugin to work
 		});
 	}
 });
