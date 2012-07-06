5908439df413404429f6daef8d457e18198b0fd5
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-06 17:09:41 +0000

    Javascript syntax improvements
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@568386 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinymce3/editor_plugin.dev.js
+++ tinymce3/editor_plugin.dev.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
 	tinymce.create('tinymce.plugins.Subscribe2Plugin', {
-		init : function(ed, url) {
+		init : function (ed, url) {
 			var pb = '<img src="' + url + '/../include/spacer.gif" class="mceSubscribe2 mceItemNoResize" />',
 			cls = 'mceSubscribe2',
 			shortcode = '[subscribe2]',
@@ -8,7 +8,7 @@
 			pbRE = new RegExp(/(\[|<!--)subscribe2.*(\]|-->)/g);
 
 			// Register commands
-			ed.addCommand('mceSubscribe2', function() {
+			ed.addCommand('mceSubscribe2', function () {
 				ed.execCommand('mceInsertContent', 0, pb);
 			});
 
@@ -20,44 +20,47 @@
 			});
 
 			// load the CSS and enable it on the right class
-			ed.onInit.add(function() {
+			ed.onInit.add(function () {
 				ed.dom.loadCSS(url + "/css/content.css");
 
 				if (ed.theme.onResolveName) {
-					ed.theme.onResolveName.add(function(th, o) {
-						if (o.node.nodeName == 'IMG' && ed.dom.hasClass(o.node, cls))
+					ed.theme.onResolveName.add(function (th, o) {
+						if (o.node.nodeName === 'IMG' && ed.dom.hasClass(o.node, cls)) {
 							o.name = 'subscribe2';
+						}
 					});
 				}
 			});
 
 			// allow selection of the image placeholder
-			ed.onClick.add(function(ed, e) {
+			ed.onClick.add(function (ed, e) {
 				e = e.target;
 
-				if (e.nodeName === 'IMG' && ed.dom.hasClass(e, cls))
+				if (e.nodeName === 'IMG' && ed.dom.hasClass(e, cls)) {
 					ed.selection.select(e);
+				}
 			});
 
 			// re-enable the CSS when the node changes
-			ed.onNodeChange.add(function(ed, cm, n) {
+			ed.onNodeChange.add(function (ed, cm, n) {
 				cm.setActive('subscribe2', n.nodeName === 'IMG' && ed.dom.hasClass(n, cls));
 			});
 
 			// create an array of replaced shortcodes so we have additional parameters
 			// then swap in the graphic
-			ed.onBeforeSetContent.add(function(ed, o) {
+			ed.onBeforeSetContent.add(function (ed, o) {
 				pbreplaced = o.content.match(pbRE);
 				o.content = o.content.replace(pbRE, pb);
 			});
 
 			// swap back the array of shortcodes to preserve parameters
 			// replace any other instances with the default shortcode
-			ed.onPostProcess.add(function(ed, o) {
+			ed.onPostProcess.add(function (ed, o) {
 				if (o.get) {
 					if ( pbreplaced !== null ) {
+						var i;
 						for ( i = 0; i < pbreplaced.length; i++ ) {
-							o.content = o.content.replace(/<img[^>]+>/, function(im) {
+							o.content = o.content.replace(/<img[^>]+>/, function (im) {
 								if (im.indexOf('class="mceSubscribe2') !== -1) {
 									im = pbreplaced[i];
 								}
@@ -65,7 +68,7 @@
 							});
 						}
 					}
-					o.content = o.content.replace(/<img[^>]+>/g, function(im) {
+					o.content = o.content.replace(/<img[^>]+>/g, function (im) {
 						if (im.indexOf('class="mceSubscribe2') !== -1) {
 							im = shortcode;
 						}
@@ -75,7 +78,7 @@
 			});
 		},
 
-		getInfo : function() {
+		getInfo : function () {
 			return {
 				longname : 'Insert Subscribe2 Token',
 				author : 'Matthew Robinson',
