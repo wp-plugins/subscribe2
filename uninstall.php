e5557d7adb6d31c3d829790f4fdbdfa6fe97fdba
MattyRob <MattyRob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-20 10:35:51 +0000

    Fix fatal error in uninstall file
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@670693 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -22,30 +22,29 @@ if ( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) {
 	} else {
 		s2_uninstall();
 	}
-
-	function s2_uninstall() {
-		global $wpdb;
-		// get name of subscribe2 table
-		$public = $wpdb->prefix . "subscribe2";
-		// delete entry from wp_options table
-		delete_option('subscribe2_options');
-		// delete legacy entry from wp-options table
-		delete_option('s2_future_posts');
-		// remove and scheduled events
-		wp_clear_scheduled_hook('s2_digest_cron');
-		// delete usermeta data for registered users
-		// use LIKE and % wildcard as meta_key names are prepended on WPMU
-		// and s2_cat is appended with category ID integer
-		$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_cat%'");
-		$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_subscribed'");
-		$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_format'");
-		$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_autosub'");
-		// delete any postmeta data that supressed notifications
-		$wpdb->query("DELETE from $wpdb->postmeta WHERE meta_key = 's2mail'");
-
-		// drop the subscribe2 table
-		$sql = "DROP TABLE IF EXISTS `" . $public . "`";
-		$wpdb->query($sql);
-	} // end s2_uninstall()
 }
+function s2_uninstall() {
+	global $wpdb;
+	// get name of subscribe2 table
+	$public = $wpdb->prefix . "subscribe2";
+	// delete entry from wp_options table
+	delete_option('subscribe2_options');
+	// delete legacy entry from wp-options table
+	delete_option('s2_future_posts');
+	// remove and scheduled events
+	wp_clear_scheduled_hook('s2_digest_cron');
+	// delete usermeta data for registered users
+	// use LIKE and % wildcard as meta_key names are prepended on WPMU
+	// and s2_cat is appended with category ID integer
+	$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_cat%'");
+	$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_subscribed'");
+	$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_format'");
+	$wpdb->query("DELETE from $wpdb->usermeta WHERE meta_key LIKE '%s2_autosub'");
+	// delete any postmeta data that supressed notifications
+	$wpdb->query("DELETE from $wpdb->postmeta WHERE meta_key = 's2mail'");
+
+	// drop the subscribe2 table
+	$sql = "DROP TABLE IF EXISTS `" . $public . "`";
+	$wpdb->query($sql);
+} // end s2_uninstall()
 ?>
