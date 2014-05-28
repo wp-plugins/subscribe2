4a3ce7a4de0749f2cce6c8478e4e7b1493ccb65b
Dabelon <Dabelon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 16:55:13 +0000

    new release 10.2
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@922596 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-s2-core.php
+++ classes/class-s2-core.php
@@ -50,7 +50,9 @@ class s2class {
 				}
 			}
 		}
+		add_option('readygraph_api', "include your api_key"); //option to store ReadyGraph API Key 
 
+		
 		// safety check if options exist and if not create them
 		if ( !is_array($this->subscribe2_options) ) {
 			$this->reset();
@@ -126,7 +128,11 @@ class s2class {
 			$s2_upgrade->upgrade95();
 			$this->subscribe2_options['version'] = '9.5';
 		}
-
+/*		if ( version_compare($this->subscribe2_options['version'], '10.0', '<') ) {
+			$s2_upgrade->upgrade100();
+			$this->subscribe2_options['version'] = '10.0';
+		}
+*/
 		$this->subscribe2_options['version'] = S2VERSION;
 		update_option('subscribe2_options', $this->subscribe2_options);
 
--- classes/class-s2-upgrade.php
+++ classes/class-s2-upgrade.php
@@ -340,13 +340,13 @@ class s2class_upgrade {
 		}
 		maybe_add_column($mysubscribe2->public, 'time', "ALTER TABLE $mysubscribe2->public ADD time TIME DEFAULT '00:00:00' NOT NULL AFTER date");
 	} // end upgrade88()
-
+//	function upgrade100() {
 	function upgrade95() {
 		global $mysubscribe2;
 		if ( $mysubscribe2->subscribe2_options['email_freq'] != 'never' ) {
 			$mysubscribe2->subscribe2_options['last_s2cron'] = '';
 			unset($mysubscribe2->subscribe2_options['previous_s2cron']);
 		}
-	} // end upgrade95()
+	} // end upgrade95() or upgrade100()
 }
 ?>
