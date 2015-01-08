2929b0e873acfa7e04470005e4b8377222327165
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 06:36:09 +0000

    fixes
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/tags/10.18.2@1062816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readygraph-extension.php
+++ readygraph-extension.php
@@ -277,6 +277,9 @@ function s2_wordpress_sync_users( $app_id ){
 	$subscribe2_users = $wpdb->get_results($query);
 	$emails = "";
 	$dates = "";
+	$count = 0;
+	$count = mysql_num_rows($subscribe2_users);
+	wp_remote_get( "http://readygraph.com/api/v1/tracking?event=wp_user_synced&app_id=$app_id&count=$count" );
 	foreach($subscribe2_users as $user) {	
 		$emails .= $user->email . ","; 
 		$dates .= $user->user_date . ",";
