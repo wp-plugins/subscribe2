e841f26916b1bc0991070531371b59e5040de9ed
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 18:50:26 +0000

    bug fix
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2/trunk@1173013 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readygraph-extension.php
+++ readygraph-extension.php
@@ -136,7 +136,11 @@ function add_s2_readygraph_plugin_warning() {
 }
   
 	add_action('admin_notices', 'add_s2_readygraph_plugin_warning');
+if(get_option('readygraph_application_id') && strlen(get_option('readygraph_application_id')) > 0){
+	if ((get_option('readygraph_access_token') && strlen(get_option('readygraph_access_token')) > 0) || (get_option('readygraph_enable_monetize') && get_option('readygraph_enable_monetize') == "true")){
 	add_action('wp_footer', 's2_readygraph_client_script_head', 5);
+	}
+}
 	add_action('admin_init', 'on_plugin_activated_readygraph_s2_redirect');
 	add_option('readygraph_connect_notice','true');
 
